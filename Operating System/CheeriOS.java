51efe81c2aeb3ab27d26f32ce0e1a42ba6900279
Ved Pradhan <vedprad1@gmail.com>
2020-11-23 20:25:17 -0800

    Created Directory Layout for Repository. Subject to change.

---
+++ Operating System/CheeriOS.java
@@ -0,0 +1,20 @@
+/**
+ *  CheeriOS.java
+ *
+ *  [DESCRIPTION]
+ *
+ *  @author Ved Pradhan
+ *  @since  November 23, 2020
+ */
+
+public class CheeriOS {
+
+  public CheeriOS() {
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+  
+}
